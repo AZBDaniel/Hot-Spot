0eb0f071e9029b622d0b33b627a3135529a5c35b
unknown <alecs2art@gmail.com>
2021-06-14 19:34:25 -0700

    need to commit to change branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,8 @@ document.getElementById("stateSelect").onchange = function() {stateDisplay()};
 function stateDisplay() {
     var place = document.getElementById("stateSelect").value;
     document.getElementById("demo").innerHTML = "you selected " + place;
+
+
+    //api call 
 }
 
