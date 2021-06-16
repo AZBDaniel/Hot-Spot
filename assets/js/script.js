c5df7d386766caee4d5ee7f5b7bd3303bfd9eafa
unknown <alecs2art@gmail.com>
2021-06-15 19:42:51 -0700

    need to commit to merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 //Javascirpt Goes Here//
+var apiKey = AIzaSyA3pyIRKeVOMy99DHETNa1VrGExhuXM0Z8
 
-document.getElementById("stateSelect").onchange = function() {stateDisplay()};
+document.getElementById("citySelect").onchange = function() {cityDisplay()};
 
-function stateDisplay() {
-    var place = document.getElementById("stateSelect").value;
+function cityDisplay() {
+    var place = document.getElementById("citySelect").value;
     document.getElementById("demo").innerHTML = "you selected " + place;
+
+    if (place === "Sedona") {
+        document.querySelector("#siteOne").innerHTML = "Sedona is a beautiful town :)";
+    }
+
 
 
     //api call 
